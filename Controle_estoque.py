1ceefedeb2e9ae46dc4609d0bcc1aec9f3bb6501
Rodrigo Lopes Velloso <velloso.rl9@outlook.com>
2025-03-13 18:24:11 -0300

    controle_estoque

--- Controle_estoque.py
+++ Controle_estoque.py
@@ -1,2 +1,30 @@
 from mold import*
 
+
+#Criando a janela
+root = tk.Tk()
+root.title("Controle de Estoque")
+root.geometry("500x300")
+root.configure(background=co0)
+root.resizable(width=False, height=False)
+#root.overrideredirect(1)
+largura_root = 500
+altura_root = 300
+#obter tamanho da tela
+largura_tela = root.winfo_screenwidth()
+altura_tela = root.winfo_screenheight()
+# Calcular posição para centralizar
+pos_x = ( largura_tela-largura_root )//2
+pos_y = (altura_tela - altura_root)//2
+# Definir geometria da janela (LxA+X+Y)
+root.geometry(f"{largura_root}x{altura_root}+{pos_x}+{pos_y}")
+
+
+
+
+
+
+
+
+
+root.mainloop()
