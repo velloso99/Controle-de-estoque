2649d5ad82d504951afea3d06af0465dd9b65a18
Rodrigo Lopes Velloso <velloso.rl9@outlook.com>
2025-03-14 11:33:41 -0300

    Controle_estoque

--- Controle_estoque.py
+++ Controle_estoque.py
@@ -95,14 +95,91 @@ def login():
     barra.place(x=170, y=275)
     porcentagem_label =Label(frame_login, text="0%", font=("Arial", 12) )
     porcentagem_label.place(x=120, y=275)
-        
-        
-
-
-
 
 def novo_usuario():
-    pass
+    
+    frame_n_senha = Frame(root, width=500, height=300, bg=co0)
+    frame_n_senha.grid(row=0, column=0, sticky=NSEW)
+    
+    def cadastrar_usuario():
+        
+        usuario = e_user.get().strip()
+        senha = e_senha.get().strip()
+    
+        # Verifica se os campos estão preenchidos
+        if not usuario or not senha:
+            messagebox.showerror("Erro", "Preencha todos os campos!")
+            return
+        # Verifica se o usuário já existe no banco
+        if verificar_usuario(usuario):
+            messagebox.showerror("Erro", "Usuário já cadastrado!")
+            return
+        for i in range(101):  # De 0 a 100
+            barra['value'] = i  # Atualiza a barra de progresso
+            porcentagem_label.config(text=f"{i}%")  # Atualiza o texto da porcentagem
+            root.update_idletasks()  # Atualiza a interface
+            root.after(5)  # Tempo de espera (20ms)
+            
+        root.destroy()
+        #subprocess.run(["python", "login.py"])
+        # Criar login no banco de dados
+        criar_login((usuario, senha))  # Passando como tupla
+
+        # Limpa os campos após o cadastro
+        e_user.delete(0, END)
+        e_senha.delete(0, END)
+        
+        
+        barra = ttk.Progressbar(frame_n_senha, length=250, mode="determinate",style="green.Horizontal.TProgressbar")
+        barra.place(x=170, y=275)
+        porcentagem_label = tk.Label(frame_n_senha, text="0%")
+        porcentagem_label.place(x=120, y=275)
+    
+        l_titulo = Label(frame_n_senha, text="Cadastrar um novo usuario", font=('Ivy 20 bold'), bg=co0, fg=co1)
+        l_titulo.place(x=230, y=15, anchor=CENTER)
+    
+        l_user = Label(frame_n_senha, text="Usuario", font=('Ivy 15 bold'), bg=co0, fg=co1)
+        l_user.place(x=240, y=60, anchor=CENTER)
+        e_user= Entry(frame_n_senha, width=25, justify=LEFT, font=('Ivy 15 bold'),  relief='solid')
+        e_user.place(x=250, y=100, anchor=CENTER)
+
+        l_senha =Label(frame_n_senha, text="Senha", font=('Ivy 15 bold'), bg=co0, fg=co1)
+        l_senha.place(x=240, y=140, anchor=CENTER)
+        e_senha= Entry(frame_n_senha, width=25, justify=LEFT, font=('Ivy 15 bold'),show="*",  relief='solid')
+        e_senha.place(x=250, y=180, anchor=CENTER)
+
+        bt_enter = Button(frame_n_senha, command=cadastrar_usuario, text="Enter", bd=3, bg=co0, fg=co1, font=('verdana', 11, 'bold'))
+        bt_enter.place(x=105, y=225)
+
+        bt_fechar = Button(frame_n_senha, command=root.destroy, text="Atualizar", bd=3, bg=co0, fg=co1, font=('verdana', 11, 'bold'))
+        bt_fechar.place(x=200, y=225)
+        
+        
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 def painel_geral():
     pass
